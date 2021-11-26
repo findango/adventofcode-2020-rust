4d666d5bb34dcc00052c2c05560eb6b99db60232
Finlay Cannon <fin@finlaycannon.com>
2021-11-25 22:23:48 -0800

    Day 1: generalize combos Still guessing what the memory ownership rules are here...

--- src/day01.rs
+++ src/day01.rs
@@ -9,22 +9,13 @@ fn parse(input: &str) -> Vec<i32> {
     input.lines().map(|l| l.parse().unwrap()).collect()
 }
 
-// TODO: make this generic so we can find any tuple based on the predicate
-fn find_pair<P>(list: &[i32], predicate: P) -> (&i32, &i32)
+fn find_combo<P>(length: usize, list: &[i32], predicate: P) -> Vec<&i32>
 where
-    P: Fn(&(&i32, &i32)) -> bool,
+    P: Fn(&Vec<&i32>) -> bool,
 {
     list.iter()
-        .tuple_combinations()
+        .combinations(length)
         .filter(predicate)
-        .next()
-        .unwrap()
-}
-
-fn find_triplet(target: i32, list: &[i32]) -> (&i32, &i32, &i32) {
-    list.iter()
-        .tuple_combinations()
-        .filter(|&(a, b, c)| a + b + c == target)
         .next()
         .unwrap()
 }
@@ -32,37 +23,34 @@ fn find_triplet(target: i32, list: &[i32]) -> (&i32, &i32, &i32) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    // use crate::utils::*;
     use std::fs::read_to_string;
 
     #[test]
     fn example_1() {
         let list = [1721, 979, 366, 299, 675, 1456];
-        let pair = find_pair(&list, |&(a, b)| a + b == 2020);
-        assert_eq!(pair, (&1721, &299));
-        assert_eq!(pair.0 * pair.1, 514579);
+        let pair = find_combo(2, &list, |xs| xs[0] + xs[1] == 2020);
+        assert_eq!(pair[0] * pair[1], 514579);
     }
 
     #[test]
     fn example_2() {
         let list = [1721, 979, 366, 299, 675, 1456];
-        let triplet = find_triplet(2020, &list);
-        assert_eq!(triplet, (&979, &366, &675));
-        assert_eq!(triplet.0 * triplet.1 * triplet.2, 241861950);
+        let triplet = find_combo(3, &list, |xs| xs[0] + xs[1] + xs[2] == 2020);
+        assert_eq!(triplet[0] * triplet[1] * triplet[2], 241861950);
     }
 
     #[test]
     fn part_1() {
         let input = read_to_string("input/input01.txt").unwrap();
         let list = parse(&input);
-        let pair = find_pair(&list, |&(a, b)| a + b == 2020);
-        assert_eq!(pair.0 * pair.1, 1015476);
+        let pair = find_combo(2, &list, |xs| xs[0] + xs[1] == 2020);
+        assert_eq!(pair[0] * pair[1], 1015476);
     }
     #[test]
     fn part_2() {
         let input = read_to_string("input/input01.txt").unwrap();
         let list = parse(&input);
-        let triplet = find_triplet(2020, &list);
-        assert_eq!(triplet.0 * triplet.1 * triplet.2, 200878544);
+        let triplet = find_combo(3, &list, |xs| xs[0] + xs[1] + xs[2] == 2020);
+        assert_eq!(triplet[0] * triplet[1] * triplet[2], 200878544);
     }
 }
